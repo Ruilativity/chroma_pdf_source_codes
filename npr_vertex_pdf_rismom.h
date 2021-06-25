d6ad1134e80169f358a937c5c21e6f8c6c111d3c
Ruilativity <41832107+Ruilativity@users.noreply.github.com>
2021-06-25 12:04:19 -0400

    Add files via upload

--- npr_vertex_pdf_rismom.h
+++ npr_vertex_pdf_rismom.h
@@ -23,7 +23,24 @@ namespace Chroma
 		 const BBLinkPattern                   LinkPattern,
 		 const char* name,
 		 const int   cfg_serial);
-
+void BkwdFrwd(const LatticePropagator&  B,
+	  const LatticePropagator&  F,
+	  general_data_base& io_output,
+	  int 		LinkNumber,
+	  int& GBB_NLinkPatterns,
+			  const multi1d< int > & LinkDirs);
+void AddLinks(const LatticePropagator&  B,
+	  const LatticePropagator&  F,
+	  const multi1d< LatticeColorMatrix > & U,
+	  multi1d< int >&    LinkDirs,
+	  const int          MaxNLinks,
+	  const int	   LinksDirection,
+	  BBLinkPattern      LinkPattern,
+	  general_data_base& io_output,
+			  int&               GBB_NLinkPatterns);
+SpinMatrixD KYToDRMat();
+void writeKYUQprop2(LatticePropagator& q, const std::string& file);
+void readKYUQprop2(LatticePropagator& q, const std::string& file);
 }  // end namespace Chroma
 
 #endif
